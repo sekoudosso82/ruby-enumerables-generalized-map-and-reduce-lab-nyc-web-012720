96614daa169dce518d0594263875d8a3f2909e44
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-04 04:46:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  l = array.length 
+  counter = 0 
+  while counter < l 
+  new_array.push(yield(array[counter]))
+  counter += 1 
+  end 
+  new_array
+end
+
+
+def reduce(array, sv = nil)
+    l = array.length
+    if sv 
+        sum = sv 
+        i = 0
+    else 
+        sum = array[0]
+        i = 1
+    end 
+    
+    while i < l
+        sum = yield(sum, array[i])
+        i += 1
+    end 
+    sum
+
+end 
+
+
